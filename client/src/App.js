c1dc85af0fcdcbeba4ae1276b7eaa348fea26801
Katherine Mu <katherinemu47@gmail.com>
2021-04-29 00:48:12 -0400

    updated spotify logo on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,5 @@
-/*
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
-
 import React, { Component } from "react";
-import spotify_logo from "./spotify_logo.png"
-import logo from "./logo.svg";
+import spotify_logo from "./spotify_logo.svg"
 import "./App.css";
 
 class App extends Component {
@@ -52,8 +23,8 @@ class App extends Component {
     return (
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to Spotify</h1>
+            <img src={spotify_logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to SpotifyExtras</h1>
           </header>
           <p className="App-intro">{this.state.apiResponse}</p>
         </div>

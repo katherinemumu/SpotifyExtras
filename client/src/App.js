b5b819fbff56d6e51edb9548a858b01786fe3ce1
Katherine Mu <katherinemu47@gmail.com>
2021-05-11 22:40:23 -0400

    created Home class, added browser router to navigate between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,18 @@
 import React, { Component } from "react";
-import spotify_logo from "./spotify_logo.svg"
 import "./App.css";
 import NavBar from "./NavBar";
+import Home from "./Home"
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res}))
-        .catch(err => err);
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
 
   render() {
     return (
-        <div className="App">
-          <NavBar />
-
-          <header className="App-header">
-            <img src={spotify_logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to SpotifyExtras</h1>
-          </header>
-          <p className="App-intro">{this.state.apiResponse}</p>
+        <div>
+            <NavBar />
+            <Switch>
+                <Route path='/' component={Home} />
+            </Switch>
         </div>
     );
   }
---
+++ client/src/Home.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import spotify_logo from "./spotify_logo.svg";
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res}))
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={spotify_logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to SpotifyExtras</h1>
+                </header>
+                <p className="App-intro">{this.state.apiResponse}</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
